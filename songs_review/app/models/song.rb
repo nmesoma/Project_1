cb21091b1a57c5d08acbfbdfc7a070c52620d996
nmesoma <christalmails@google.com>
2015-04-14 13:29:33 +0100

    Audio uploaded in my app and reveiw model generated, user and review model association done

--- songs_review/app/models/song.rb
+++ songs_review/app/models/song.rb
@@ -1,5 +1,6 @@
 class Song < ActiveRecord::Base
   belongs_to :user
   mount_uploader :image, SongImageUploader
+  mount_uploader :file, AudioUploader
 end
 
--- songs_review/db/schema.rb
+++ songs_review/db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150413115724) do
+ActiveRecord::Schema.define(version: 20150414115518) do
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer  "rating"
+    t.text     "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+  end
 
   create_table "songs", force: :cascade do |t|
     t.string   "title"
