41e615b3a1ecc2088fa1d452b67c7e748e654ecb
nmesoma <christalmails@google.com>
2015-04-15 10:43:58 +0100

    all three models working and liked review page worknig and linked to the profile page

--- songs_review/app/controllers/reviews_controller.rb
+++ songs_review/app/controllers/reviews_controller.rb
@@ -1,17 +1,18 @@
 class ReviewsController < ApplicationController
   before_action :set_review, only: [:show, :edit, :update, :destroy]
+  before_action :set_song
   before_action :authenticate_user!
 
-  # GET /reviews
-  # GET /reviews.json
-  def index
-    @reviews = Review.all
-  end
+  # # GET /reviews
+  # # GET /reviews.json
+  # def index
+  #   @reviews = Review.all
+  # end
 
-  # GET /reviews/1
-  # GET /reviews/1.json
-  def show
-  end
+  # # GET /reviews/1
+  # # GET /reviews/1.json
+  # def show
+  # end
 
   # GET /reviews/new
   def new
@@ -27,10 +28,11 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
     @review.user_id = current_user.id
+    @review.song_id = @song.id
 
     respond_to do |format|
       if @review.save
-        format.html { redirect_to @review, notice: 'Review was successfully created.' }
+        format.html { redirect_to @song }
         format.json { render :show, status: :created, location: @review }
       else
         format.html { render :new }
@@ -44,7 +46,7 @@ class ReviewsController < ApplicationController
   def update
     respond_to do |format|
       if @review.update(review_params)
-        format.html { redirect_to @review, notice: 'Review was successfully updated.' }
+        format.html { redirect_to @review }
         format.json { render :show, status: :ok, location: @review }
       else
         format.html { render :edit }
@@ -62,15 +64,16 @@ class ReviewsController < ApplicationController
       format.json { head :no_content }
     end
   end
-  # reviews_url,
+  
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_review
       @review = Review.find(params[:id])
     end
+
     def set_song
       @song = Song.find(params[:song_id])
-
     end 
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- songs_review/db/schema.rb
+++ songs_review/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150414115518) do
+ActiveRecord::Schema.define(version: 20150415092612) do
 
   create_table "reviews", force: :cascade do |t|
     t.integer  "rating"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20150414115518) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.integer  "song_id"
   end
 
   create_table "songs", force: :cascade do |t|
