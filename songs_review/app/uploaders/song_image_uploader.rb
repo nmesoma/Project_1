652661d0596954a55a208fa7ab93ed67a769fe8a
nmesoma <christalmails@google.com>
2015-04-16 13:14:22 +0100

    bacground image added

--- songs_review/app/uploaders/song_image_uploader.rb
+++ songs_review/app/uploaders/song_image_uploader.rb
@@ -32,14 +32,13 @@ class SongImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-   version :thumb do
-  process :resize_to_fill => [100, 100]
-   end
-   
+  version :thumb do
+    process :resize_to_fill => [250, 250]
+  end
 
-   version :detail do
-     process :resize_to_fit => [500, 500]
-   end
+  version :detail do
+    process :resize_to_fit => [500, 500]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
