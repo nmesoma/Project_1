da05489ea18eef5413f06d38f136360202af5b53
nmesoma <christalmails@google.com>
2015-04-16 17:22:15 +0100

    home page css done and completed

--- songs_review/app/controllers/application_controller.rb
+++ songs_review/app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :create_search_object
+
+  def create_search_object
+    @search = Song.search(params[:q])
+  end
 end
